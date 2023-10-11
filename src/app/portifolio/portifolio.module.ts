90593550852a50b9832bae247cd04d478871f7f9
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-10 22:59:27 -0300

    feat: adicionando componente home

--- src/app/portifolio/portifolio.module.ts
+++ src/app/portifolio/portifolio.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PortifolioRoutingModule } from './portifolio-routing.module';
+import { HomePageComponent } from './containers/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
     PortifolioRoutingModule
