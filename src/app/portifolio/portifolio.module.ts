8f1d152e8bb586bf327f996b6d61ccab63037e81
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-10 22:55:44 -0300

    feat: adicionando modulo

---
+++ src/app/portifolio/portifolio.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PortifolioRoutingModule } from './portifolio-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PortifolioRoutingModule
+  ]
+})
+export class PortifolioModule { }
